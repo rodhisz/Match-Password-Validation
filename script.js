c149ee1cec889d952329935e9c7d1659ed4de08e
rodhisz <rodhisz.45@gmail.com>
2022-05-25 14:24:25 +0700

    add js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-var check = function() {  
-    if (document.getElementById('password').value ==  
-      document.getElementById('checkPassword').value) {  
-      document.getElementById('alertPassword').style.color = '#8CC63E';  
-      document.getElementById('alertPassword').innerHTML = '<span><i class="fas fa-check-circle"></i>Match !</span>';  
-    } else {  
-              document.getElementById('alertPassword').style.color = '#EE2B39';  
-      document.getElementById('alertPassword').innerHTML = '<span><i class="fas fa-exclamation-triangle"></i>not matching</span>';  
-    }  
-  } 
+var check = function () {
+  if (document.getElementById('password').value == 
+  document.getElementById('checkPassword').value) {
+    document.getElementById('alertPassword').style.color = '#8CC63E';
+    document.getElementById('alertPassword').innerHTML = '<span> <i class = "fas fa-check-circle"></i>Match !</span>';
+  } else {
+    document.getElementById('alertPassword').style.color = '#EE2B39';
+    document.getElementById('alertPassword').innerHTML = '<span> <i class = "fas fa-exclamation-triangle"></i>Not Match !</span>';
+  }
+}
